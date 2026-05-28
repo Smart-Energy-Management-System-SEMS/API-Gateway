3b8327dea9cc19aa3c2dae3caa287b041481d11d
krxxg <u202312629@upc.edu.pe>
2026-05-27 21:39:57 -0500

    feat: create API Gateway project

---
+++ src/main/java/com/sems/apigateway/ApiGatewaySemsApplication.java
@@ -0,0 +1,13 @@
+package com.sems.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewaySemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewaySemsApplication.class, args);
+    }
+
+}
